203d837f576b941d2b8279cc4ef5966173097a1e
Tamer Shlash <mr.tamershlash@gmail.com>
2016-12-13 04:16:29 +0800

    added youtube initializer

--- config/initializers/youtube.rb
+++ config/initializers/youtube.rb
@@ -2,4 +2,4 @@ require 'google/apis/youtube_v3'
 
 YT = Google::Apis::YoutubeV3
 Youtube = YT::YouTubeService.new
-Youtube.key = ENV.fetch('YOUTUBE_API_KEY')
+Youtube.key = ENV.fetch('YOUTUBE_API_KEY', 'AIzaSyCANIpOUZd0-DKliNE_--9G_WyEdyLOiOg')
